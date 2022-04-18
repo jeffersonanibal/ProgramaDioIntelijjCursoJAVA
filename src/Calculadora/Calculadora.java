3788b9c9c55d1b2b1aba6c8f2b5fce0d8d06aa6e
jefferson <contadojeff01@gmail.com>
2022-04-18 11:19:10 -0300

    Calculadora...

---
+++ src/Calculadora/Calculadora.java
@@ -0,0 +1,41 @@
+package Calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int a, b;
+
+        System.out.println("Digite o primeiro numero: ");
+        a = scan.nextInt();
+        System.out.println("Digite o segundo numero: ");
+        b = scan.nextInt();
+
+        int somar = somar(a, b);
+        int subtrair = subtrair(a, b);
+        int multiplicar = multiplicar(a, b);
+        double dividir = dividir(a, b);
+
+        System.out.println("somar " + somar);
+        System.out.println("subtrair " + subtrair);
+        System.out.println("multiplicar " + multiplicar);
+        System.out.println("dividir " + dividir);
+
+    }
+
+    public static int somar(int a , int b) {
+        return a + b;
+    }
+    public static int subtrair(int a , int b) {
+        return a - b;
+    }
+    public static int multiplicar(int a , int b) {
+        return a * b;
+    }
+    public static double dividir(int a , int b) {
+        return a / b;
+    }
+}
